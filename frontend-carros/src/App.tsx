ac3d5ef40341309d643ae20e6cf82c91506a318f
Wagner Rodriges <felten19@gmail.com>
2022-11-10 14:31:20 -0300

    Initial commit

---
+++ frontend-carros/src/App.tsx
@@ -0,0 +1,11 @@
+import './assets/Style/custon.scss';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ frontend-carros/src/components/Navbar/index.tsx
@@ -0,0 +1,41 @@
+import "bootstrap/js/src/collapse.js";
+import "./Style.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md navbar-dark bg-warning main-nav">
+        <div className="container-fluid">
+          <a href="link">
+            <h1>Revenda DevSuper</h1>
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbar-hamburg"
+            aria-controls="dscatalog-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbar-hamburg">
+            <ul>
+              <li>
+                <a href="link">Home</a>
+              </li>
+              <li>
+                <a href="link">Catalógo</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
