341899dcdfd549d4325c23a1cb0b42519ccaa189
WagnerRodrigues <felten19@gmail.com>
2022-11-23 19:51:13 -0300

    terminado o satyle do card de venda dos carros

--- frontend-carros/src/components/Card/card.tsx
+++ frontend-carros/src/components/Card/card.tsx
@@ -1,24 +1,26 @@
 import  Car from '../../img/car-card.png';
-
+import "./style.css"
 
 const Card = () => {
   return (
     <>
-      <div>
-        <div>
+      <div className='card-container'>
+      <div className='car'>
+            <img src={Car} alt="Audio TT" />
+        </div>
+
+        <div className='card-tittle'>
           <h1>Audio supra TT</h1>
           <p>
-            motor montado transversalmente, e uma transmissão de 5 velocidades
+            Motor montado transversalmente, e uma transmissão de 5 velocidades
             manual para motores de 180 cv (132 kW), 6 velocidades manual para
             motores de 225 cv (165 kW)
           </p>
         </div>
 
-        <div>
-            <img src={Car} alt="Audio TT" />
-        </div>
+        
 
-        <div>
+        <div className='card-purchase'>
             <a href="link">Comprar</a>
         </div>
       </div>
