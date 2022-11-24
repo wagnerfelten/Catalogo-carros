cbc775323a48e54f03891bc00b058a1cd2b0e3fc
WagnerRodrigues <felten19@gmail.com>
2022-11-24 14:55:39 -0300

    criado Grid System dos cards

--- frontend-carros/src/pages/Catalog/index.tsx
+++ frontend-carros/src/pages/Catalog/index.tsx
@@ -1,23 +1,47 @@
 import Card from "../../components/Card/card";
 import Navbar from "../../components/Navbar";
-import './style.css';
-
+import carRed from "../../img/car-card.png";
+import "./style.css";
 
 function Catalogo() {
-    return (
-      <>
-        <Navbar />
+  return (
+    <>
+      <Navbar />
 
-        <div>
-            <div className="search">
-                <input type="text" name="" id="" placeholder="Digite sua busca" />
-                <a href="link">Buscar</a>
+      <div>
+        <div className="search">
+          <input type="text" name="" id="" placeholder="Digite sua busca" />
+          <a href="link">Buscar</a>
+        </div>
+
+        <div className="container my-4">
+          <div className="row">
+            <div className="col-sm-6 col-lg-4">
+              <Card />
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <Card />
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <Card />
+            </div>
+            <div className="col-sm-6 col-lg-4">
+              <Card />
             </div>
 
-            <Card />
+            <div className="col-sm-6 col-lg-4">
+              <Card />
+            </div>
+
+            <div className="col-sm-6 col-lg-4">
+              <Card />
+            </div>
+           
+          </div>
         </div>
-      </>
-    );
-  }
-  
-  export default Catalogo;
+      </div>
+    </>
+  );
+}
+
+export default Catalogo;
